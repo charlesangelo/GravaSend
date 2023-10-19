8d093288817806ec2d1c05db73ff0fb9dd61787b
charlesangelo <142655362+charlesangelo@users.noreply.github.com>
2023-10-19 20:56:13 +0800

    ALL WORKING ONLY NEED TO FIX XML ON SPEEDTRACKER AND LOCATION

--- app/src/main/java/com/example/gravasend/SafetyChecklist.java
+++ app/src/main/java/com/example/gravasend/SafetyChecklist.java
@@ -34,7 +34,6 @@ import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 public class SafetyChecklist extends AppCompatActivity {
-    // Define the UI elements
     private ImageButton backButton;
     private Button saveButton, clearButton, selectImageButton;
     private EditText plateNumberInput, odometerInput, imageDescriptionInput;
@@ -45,7 +44,6 @@ public class SafetyChecklist extends AppCompatActivity {
     private Uri selectedImageUri;
     private String imageUrl;
 
-    // Request code for image selection
     private static final int PICK_IMAGE_REQUEST = 1;
 
     @Override
@@ -53,22 +51,18 @@ public class SafetyChecklist extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.safetychecklist);
 
-        // Initialize Firebase Authentication
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
 
         if (currentUser == null) {
-            // User is not authenticated, redirect to login or sign-up
             startActivity(new Intent(SafetyChecklist.this, MainActivity.class));
             finish();
             return;
         }
 
-        // Initialize Firebase Database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         userReference = database.getReference("SafetyChecklist").child(currentUser.getUid());
 
-        // Initialize views
         backButton = findViewById(R.id.backButton);
         saveButton = findViewById(R.id.button4);
         clearButton = findViewById(R.id.button3);
@@ -79,10 +73,8 @@ public class SafetyChecklist extends AppCompatActivity {
         checkBox = findViewById(R.id.checkBox);
         imagePlaceholder = findViewById(R.id.imagePlaceholder);
 
-        // Load existing data when the activity starts
         loadUserData();
 
-        // Handle the "Back" button click
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,7 +83,6 @@ public class SafetyChecklist extends AppCompatActivity {
             }
         });
 
-        // Handle the "Save" button click
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,7 +90,6 @@ public class SafetyChecklist extends AppCompatActivity {
             }
         });
 
-        // Handle the "Clear" button click
         clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -107,7 +97,6 @@ public class SafetyChecklist extends AppCompatActivity {
             }
         });
 
-        // Set an onClickListener for the imagePlaceholder
         selectImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -116,30 +105,19 @@ public class SafetyChecklist extends AppCompatActivity {
         });
     }
 
-    // Inside loadUserData method
     private void loadUserData() {
-        // Read data from Firebase and populate UI fields
         userReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull com.google.firebase.database.DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     UserData userData = dataSnapshot.getValue(UserData.class);
-
-                    // Populate UI fields with loaded data
                     plateNumberInput.setText(userData.getPlateNumber());
                     odometerInput.setText(userData.getOdometerReading());
                     imageDescriptionInput.setText(userData.getImageDescription());
                     checkBox.setChecked(userData.isChecked());
-
-                    // Load the image URL
                     imageUrl = userData.getImageUrl();
-
-                    // Load the image if the URL exists
                     if (imageUrl != null && !imageUrl.isEmpty()) {
-                        // Clear Picasso's memory cache
                         Picasso.get().invalidate(imageUrl);
-
-                        // Load the new image using Picasso
                         Picasso.get().load(imageUrl).into(imagePlaceholder);
                     }
                 }
@@ -158,10 +136,8 @@ public class SafetyChecklist extends AppCompatActivity {
         String imageDescription = imageDescriptionInput.getText().toString();
         boolean isChecked = checkBox.isChecked();
 
-        // Create a data object and save it to Firebase
         UserData userData = new UserData(plateNumber, odometerReading, imageDescription, isChecked);
 
-        // Set the existing image URL if it exists (to retain the image)
         if (imageUrl != null && !imageUrl.isEmpty()) {
             userData.setImageUrl(imageUrl);
         }
@@ -172,40 +148,32 @@ public class SafetyChecklist extends AppCompatActivity {
     }
 
     private void clearUserData() {
-        // Clear all UI fields
         plateNumberInput.setText("");
         odometerInput.setText("");
         imageDescriptionInput.setText("");
         checkBox.setChecked(false);
 
-        // Clear the data in Firebase by setting it to null
         userReference.setValue(null);
 
-        // Clear the imagePlaceholder
         imagePlaceholder.setImageResource(R.drawable.ic_launcher_foreground);
 
-        // Check if an image was previously uploaded
         if (imageUrl != null && !imageUrl.isEmpty()) {
-            // Delete the image from Firebase Storage
             FirebaseStorage storage = FirebaseStorage.getInstance();
             StorageReference storageRef = storage.getReferenceFromUrl(imageUrl);
 
             storageRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void aVoid) {
-                    // Image deleted successfully from Firebase Storage
-                    imageUrl = ""; // Set imageUrl to an empty string
+                    imageUrl = "";
                     Toast.makeText(SafetyChecklist.this, "Image cleared successfully.", Toast.LENGTH_SHORT).show();
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
-                    // Handle the error
                     Toast.makeText(SafetyChecklist.this, "Failed to clear image.", Toast.LENGTH_SHORT).show();
                 }
             });
         } else {
-            // No image to clear, just set imageUrl to an empty string
             imageUrl = "";
         }
 
@@ -215,21 +183,19 @@ public class SafetyChecklist extends AppCompatActivity {
     private void checkAndRequestPermissions() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                // Permission not granted, request it
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PICK_IMAGE_REQUEST);
             } else {
-                // Permission already granted, launch image picker
                 launchImagePicker();
             }
         } else {
-            // Launch image picker (no need to request permissions on older Android versions)
             launchImagePicker();
         }
     }
 
     private void launchImagePicker() {
-        // Launch the image picker
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
@@ -238,14 +204,10 @@ public class SafetyChecklist extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
-            // User has successfully picked an image
             selectedImageUri = data.getData();
 
-            if (selectedImageUri != null) { // Check if selectedImageUri is not null
-                // Set the selected image to the imagePlaceholder ImageView
+            if (selectedImageUri != null) {
                 imagePlaceholder.setImageURI(selectedImageUri);
-
-                // Upload the image to Firebase Storage and save its URL in Firebase Realtime Database
                 uploadImageToFirebase(selectedImageUri);
             } else {
                 Toast.makeText(SafetyChecklist.this, "Failed to get image URI.", Toast.LENGTH_SHORT).show();
@@ -257,13 +219,10 @@ public class SafetyChecklist extends AppCompatActivity {
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
 
         if (currentUser == null) {
-            // User is not authenticated, handle accordingly
             return;
         }
 
         String userUid = currentUser.getUid();
-
-        // Create a unique filename for the image using the current timestamp
         String timestamp = String.valueOf(System.currentTimeMillis());
         String imageFilename = userUid + "_" + timestamp + "_" + imageUri.getLastPathSegment();
 
@@ -275,18 +234,12 @@ public class SafetyChecklist extends AppCompatActivity {
         uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                // Image uploaded successfully, get the download URL
                 storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                     @Override
                     public void onSuccess(Uri uri) {
                         imageUrl = uri.toString();
-
-                        // Log the imageUrl to check if it's correct
                         Log.d("ImageUrl", imageUrl);
-
-                        // Update the user's data with the image URL
-                        saveUserData(); // Save the user data again to include the image URL
-
+                        saveUserData();
                         Toast.makeText(SafetyChecklist.this, "Image uploaded successfully.", Toast.LENGTH_SHORT).show();
                     }
                 });
@@ -294,7 +247,6 @@ public class SafetyChecklist extends AppCompatActivity {
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                // Handle the error
                 Toast.makeText(SafetyChecklist.this, "Image upload failed.", Toast.LENGTH_SHORT).show();
             }
         });
